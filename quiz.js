2a3415c1a420144c2d389bfe12b885ab133140fc
Ma11 <36732487+maxcrup007@users.noreply.github.com>
2020-09-16 13:46:34 +0700

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,10 @@
+let max = {
+    name: 'Vorawut',
+    lastname: 'Phirunpatthanagul',
+    nickname: 'Max',
+    age: 21,
+    habbit: 'play games',
+    Phonenumber: '084-4877396',
+    gender: 'male'
+}
+console.log(max)
