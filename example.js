99f92180a53f1c14498bae6160cd7ed69294c294
Ma11 <36732487+maxcrup007@users.noreply.github.com>
2020-09-16 13:39:16 +0700

    Create example.js

---
+++ example.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+const PI = 3.1415926
+
+console.log("PI:",PI)
+
+let name = "John"
+name = "JOJO"
+let test = "Hi there"
+name = `${test} B`
+
+console.log("name: ", name)
+
+let person = {
+    name: 'JOJO',
+    age: 15,
+    gender: 'male'
+}
+console.log(person)
