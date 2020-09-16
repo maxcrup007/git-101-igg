6b3e531f3819d08bca562dc8247999fa5a852c4d
Ma11 <36732487+maxcrup007@users.noreply.github.com>
2020-09-16 14:09:34 +0700

    Add functiona
    diff'

---
+++ function.js
@@ -0,0 +1,19 @@
+// function add(a, b) {
+//     return a + b
+// }
+
+// function nul(a,b) {
+//     return a * b
+// }
+
+// function searchCustomer(customerName) {
+    
+// }
+
+const add = (a,b) => a + b
+
+const nul = (a,b) => a * b
+
+
+console.log(add(2,3))
+console.log(nul(2,3))
