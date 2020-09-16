bce363401dec4cfb80986409d65a0c4936e748b0
PupakornChutuad <37680367+PupakornChutuad@users.noreply.github.com>
2020-09-16 13:50:29 +0700

    hgg

---
+++ Quiz1.js
@@ -0,0 +1,10 @@
+let identity = {
+    firstname : 'Pupakorn',
+    lastname : 'Chutuad',
+    age: '21',
+    hobbie:'play games',
+    tel:'0000000000'
+
+
+}
+console.log(identity)
---
+++ example.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+
+const PI = 3.14
+console.log("PI: ",PI)
